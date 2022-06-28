9ef41454ac400b91ff2f04d2c4e94143896cfc3f
Violeta Gonzalez G <violeta@casita.goxoa>
2022-06-28 11:02:12 +0200

    mejorado basicsh

--- basic.sh
+++ basic.sh
@@ -5,21 +5,21 @@ source Opte.fu
 source Continua.fu
 source Echocolors.fu # black red green yellow blue magenta cyan white pink orange whiteguay @+N
 
-pdftotext $profile.pdf 
+#pdftotext $profile.pdf 
+#
+#sed -i "/Capítulo/i\
+#hastaaqui
+#" $profile.txt
 
-sed -i "/Capítulo/i\
-hastaaqui
-" $profile.txt
-
-awk -f split_a_file.awk $profile.txt
+#awk -f split_a_file.awk $profile.txt
 
 for e in $(seq 10)
 do
+	ejercicio="ejercicio.${e}" 
 	let e=${e}-1 
 	edir="ex0${e}" 
 	mkdir ${edir} 
-	enunciado="enunciado_${e}"
-	vim ${enunciado} 
+	vim ${ejercicio}  
 	continua
 done	
     
