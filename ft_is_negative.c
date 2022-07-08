ca6c6d2632775ffadf89ce8a5030ef2f386fb977
Violeta Gonzalez garcia <viogonza@c1r3s2.42urduliz.com>
2022-07-08 15:10:27 +0200

    ex300

--- ft_is_negative.c
+++ ft_is_negative.c
@@ -6,10 +6,10 @@
 /*   By: viogonza <viogonza@student.42urduliz.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:14:46 by viogonza          #+#    #+#             */
-/*   Updated: 2022/07/08 15:03:17 by viogonza         ###   ########.fr       */
+/*   Updated: 2022/07/08 14:35:51 by viogonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stdio.h>
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -19,66 +19,29 @@ void	ft_putchar(char c)
 
 void	ft_is_negative(int n)
 {
-	int	upper_limit;
-	int	lower_limit;
-
-	upper_limit = 2147483647;
-	lower_limit = -2147483648;
-
-	if ((n%2 = 0) || (n%2 = 1))
-	{
-
-
-
-
-
-
-
-
-	if (n <= 2147483647 && n > -2147483648)
-	{
-
-		if ( n == -2147483648)
-		{
-			ft_putchar('?');
-			printf("%d",n);
-			prueba = n + 2147483647;
-			printf("%d",prueba);
-
-			return;
-		}
-		
-
 		if (n < 0)
 		{
 			ft_putchar('N');
-			ft_putchar(10);
-		}
-		else if (n > 0)
-		{
-			ft_putchar('P');
-			ft_putchar(10);
 		}
 		else
 		{
-			ft_putchar('E');
+			ft_putchar('P');
 		}
-	}
 }
 
 int main() {
-	int p = 2147483647;
-	int n = -2147483647;
-	int prueba;
 
-	
-	prueba = n + p;
+    int a = 2147483640;
+    int b = -2147483640;
+	int rest = 0;
 
-	printf("%d",prueba);
-	
-	
-	ft_putchar(10);
-	ft_is_negative(p);
-	ft_putchar(10);
-	ft_is_negative(n);
+
+    while ( a>=0 && b<=0 )
+    {
+			rest = a - b;
+            ft_is_negative(b);
+			printf("%d\n",b);
+            a++;
+            b--;
+    }
 }
